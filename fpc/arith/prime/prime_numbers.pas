a1204fab2ab2cf9bc1db5af0d5dc1b573b97d362
wp_xyz <wp_xxyyzzz@gmx.net>
2025-05-25 11:15:05 +0200

    Use mod operator in arith/prime

--- fpc/arith/prime/prime_numbers.pas
+++ fpc/arith/prime/prime_numbers.pas
@@ -1,24 +1,27 @@
-{********************************************
-*              PRIME NUMBERS                *
-* ----------------------------------------- *
-* This small program tests if a given inte- *
-* ger number is a prime number or not.      *
-* ----------------------------------------- *
-* Ref.: "Mathématiques par l'informatique   *
-*        individuelle (1) par H. Lehning    *
-*        et D. Jakubowicz, Masson, Paris,   *
-*        1982" [BIBLI 06].                  *
-*                                           *
-*            Pascal version By J-P Moreau.  *
-*                  (www.jpmoreau.fr)        *
-*********************************************
+{******************************************************************
+*                         PRIME NUMBERS                           *
+* --------------------------------------------------------------- *
+* This small program tests if a given integer number is a prime   *
+* number or not.                                                  *
+* --------------------------------------------------------------- *
+* Ref.: "Mathématiques par l'informatique individuelle (1)        *
+*         par H. Lehning et D. Jakubowicz, Masson, Paris, 1982"   *
+*         [BIBLI 06].                                             *
+*                                                                 *
+*                                  Pascal version By J-P Moreau.  *
+*                                        (www.jpmoreau.fr)        *
+* --------------------------------------------------------------- *
+* Modernized version for FreePascal/Lazarus:                      *
+* - Make cross-patform by removing WinCRT                         *
+* - Remove goto instructions and labels                           *
+* - Use mod operator to check divisibility --> clearer code.      *
+*******************************************************************
 See also program factors.pas}
 program prime_numbers;
 
-Label 100, fin;
-
 const
   EPS = 1e-6;
+
 var
   d: Real;
   i, m, n: Integer;
@@ -43,16 +46,22 @@ begin
   WriteLn;
 
   {Test if multiple of 2}
+  {
   d := n - 2*INT(n/2);
   if abs(d)<eps then
+  }
+  if n mod 2 = 0 then
   begin
     NoPrime;
     exit;
   end;
 
   {Test if multiple of 3}
+  {
   d := N - 3*INT(n/3);
   if abs(d)<eps then
+  }
+  if n mod 3 = 0 then
   begin
     NoPrime;
     exit;
@@ -63,19 +72,25 @@ begin
   m := trunc(sqrt(n)) + 1;
   while i <= m do
   begin
+  {
     d := n - (i-1)*INT(n/(i-1));
     if abs(d)<eps then
+  }
+    if n mod (i-1) = 0 then
     begin
       NoPrime;
       exit;
     end;
+    {
     d := n - (i+1)*INT(n/(i+1));
     if abs(d)<eps then
+    }
+    if n mod (i+1) = 0 then
     begin
       NoPrime;
       exit;
     end;
-    i := i+6
+    i := i + 6;
   end;
   WriteLn(' Prime number.');
   Pause;
