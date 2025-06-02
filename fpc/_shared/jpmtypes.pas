63ba61389a79b26167310d9d7742cb9eab1c103d
wp_xyz <wp_xxyyzzz@gmx.net>
2025-06-02 22:09:07 +0200

    Add Gauss Integration

--- fpc/_shared/jpmtypes.pas
+++ fpc/_shared/jpmtypes.pas
@@ -7,8 +7,11 @@ interface
 type
   float = double;
 
-  TFunction1 = function(x: Float): float;
-  TFunction2 = function(x, y: Float): float;
+  TFunction1 = function(x: float): float;
+  TFunction2 = function(x, y: float): float;
+  TFunction3 = function(x, y, z: float): float;
+
+  TFloatArray = array of float;
 
 const
   NaN = 1.0/0.0;
--- fpc/_unittests/jpmintegrationtests.pas
+++ fpc/_unittests/jpmintegrationtests.pas
@@ -11,6 +11,7 @@ type
 
   TTestIntegrations = class(TTestCase)
   published
+    procedure TestGaussIntegration;
     procedure TestRombergIntegration;
   end;
 
@@ -27,6 +28,67 @@ end;
 function SinExpFunc(x: double): Double;
 begin
   Result := sin(x) * exp(-x);
+end;
+
+function XFUnc(x: Double): Double;
+begin
+  Result := x;
+end;
+
+function XYFunc(x, y: Double): Double;
+begin
+  Result := x * y;
+end;
+
+function XYZFunc(x, y, z: Double): Double;
+begin
+  Result := x * y * z;
+end;
+
+function SinXYZFunc(x, y, z: Double): Double;
+begin
+  Result := sin(x * y * z);
+end;
+
+procedure TTestIntegrations.TestGaussIntegration;
+const
+  EPS = 1E-10;
+var
+  n: Integer;
+  actual, expected: Double;
+begin
+  // Integral of x dx between x=0..1
+  expected := 0.5;
+  for n := 2 to 10 do
+  begin
+    actual := GaussIntegral(@XFunc, n, 0,1);
+    CheckEquals(expected, actual, EPS, 'Gauss Integral f(x)=x, order ' + IntToStr(n) + ': Result mismatch');
+  end;
+
+  // Integral x*y dxdy between x=0..1 and y=0..1
+  expected := 0.25;
+  for n := 2 to 10 do
+  begin
+    actual := GaussIntegral(@XYFunc, n , 0,1, 0,1);
+    CheckEquals(expected, actual, EPS, 'Gauss Integral f(x,y)=x*y, order ' + IntToStr(n) + ': Result mismatch');
+  end;
+
+  // Integral x*y*z dxdydz between x=0..1 and y=0..1 and z=0..1
+  expected := 0.125;
+  for n := 2 to 10 do
+  begin
+    actual := GaussIntegral(@XYZFunc, n , 0,1, 0,1, 0,1);
+    CheckEquals(expected, actual, EPS, 'Gauss Integral f(x,y,z)=x*y*z, order ' + IntToStr(n) + ': Result mismatch');
+  end;
+
+  // Integral sin(x*y*z) dxdydz between x=0..1 and y=0..1 and z=0..1
+  expected := 0.1224345;  // result provided by Wolfram Alpha
+  for n := 2 to 10 do
+  begin
+    actual := GaussIntegral(@SinXYZFunc, n , 0,1, 0,1, 0,1);
+    CheckEquals(expected, actual, 1E-5, 'Gauss Integral f(x,y,z)=sin(x*y*z), order ' + IntToStr(n) + ': Result mismatch');
+  end;
+
 end;
 
 procedure TTestIntegrations.TestRombergIntegration;
